f1e53ef99f0cd677ec08b8adf1d225637a11c329
Bytestring0 <Au2450126071@outlook.com>
2025-11-12 12:50:59 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 export default defineConfig({
 	site: "https://bytestring.top",
 
-	base: "/",
+	base: "",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
