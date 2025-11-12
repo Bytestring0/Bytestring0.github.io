05651ee03641eefa658a91d0d4a6a74b2632f944
Bytestring0 <Au2450126071@outlook.com>
2025-11-12 12:11:09 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://Bytestring0.github.io",
+	site: "https://bytestring.top",
 
 	base: "/",
 	trailingSlash: "always",
